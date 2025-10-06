cdf6f5798b457b736c2b5b2b21be2f14a7cd1c77
Artyom Nikitin <artemf642@gmail.com>
2025-10-06 15:12:00 +0300

    Added `server_config.go` configuration

--- services/simple-http-server/src/server/server.go
+++ services/simple-http-server/src/server/server.go
@@ -1,14 +1,18 @@
 package server
 
 import (
+	"HttpServer/configs"
 	"HttpServer/server/handlers"
+	"fmt"
 	"net/http"
+	"strconv"
 )
 
 type MyServer struct {
 	*http.Server
 }
 
-func (s *MyServer) StartServer() error {
-	return http.ListenAndServe(":5252", GetRouter(&handlers.MyHandler{}))
+func (s *MyServer) StartServer(config *configs.ServerConfig) error {
+	fmt.Printf("Starting server on port %v", config.Server.Port)
+	return http.ListenAndServe(":"+strconv.Itoa(config.Server.Port), GetRouter(&handlers.MyHandler{}))
 }
