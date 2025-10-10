8c30fcc0c421bdbd584066cae090c635c0ae8f2e
Artyom Nikitin <artemf642@gmail.com>
2025-10-10 13:14:38 +0300

    fix

--- services/simple-http-server/src/main.go
+++ services/simple-http-server/src/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	cfg, err := config.LoadServerConfig("services/simple-http-server/config/server_config.yaml")
+	cfg, err := config.LoadServerConfig("C:\\Users\\zxcah\\GolandProjects\\awesomeProject1\\services\\simple-http-server\\config\\server_config.yaml")
 	if err != nil {
 		log.Fatalf("Config loading error: %v", err)
 	}
--- services/simple-http-server/src/server/server.go
+++ services/simple-http-server/src/server/server.go
@@ -1,14 +1,16 @@
 package server
 
 import (
+	"HttpServer/config"
 	"HttpServer/server/handlers"
 	"net/http"
+	"strconv"
 )
 
 type MyServer struct {
 	*http.Server
 }
 
-func (s *MyServer) StartServer() error {
-	return http.ListenAndServe(":5252", GetRouter(&handlers.MyHandler{}))
+func (s *MyServer) StartServer(cfg *config.ServerConfig) error {
+	return http.ListenAndServe(":"+strconv.Itoa(cfg.Server.Port), GetRouter(&handlers.MyHandler{}))
 }
