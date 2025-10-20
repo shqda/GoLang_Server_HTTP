cb97700bdb68300cb1cf3f8bd39a9b08a64f4fb5
Artyom Nikitin <artemf642@gmail.com>
2025-10-20 16:02:28 +0300

    func renamed

--- services/simple-http-server/src/server/server.go
+++ services/simple-http-server/src/server/server.go
@@ -11,5 +11,5 @@ type MyServer struct {
 }
 
 func (s *MyServer) StartServer(cfg *config.ServerConfig) error {
-	return http.ListenAndServe(cfg.Server.Host+":"+cfg.GetServerPortAsString(), GetRouter(&handlers.MyHandler{}))
+	return http.ListenAndServe(cfg.Server.Host+":"+cfg.GetServerPort(), GetRouter(&handlers.MyHandler{}))
 }
